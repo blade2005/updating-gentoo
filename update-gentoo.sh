6bf225ab0397ae8ef0de60c350b37b85b9d6cedb
Craig Davis <cdavis@stoneydavis.com>
2018-10-22 21:00:44 -0500

    fixup calling the scripts right.

--- update-gentoo.sh
+++ update-gentoo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-01-sync-portage.sh && \
-02-update-portage.sh && \
-03-update-gcc.sh && \
-04-update-world.sh && \
-05-depclean.sh && \
-06-misc.sh
+./01-sync-portage.sh && \
+./02-update-portage.sh && \
+./03-update-gcc.sh && \
+./04-update-world.sh && \
+./05-depclean.sh && \
+./06-misc.sh
 
